bf6376c223daa25bda491641b686e40c70e2b609
Samuel Bruin <S.bruin-1@student.tudelft.nl>
2024-09-27 21:26:21 +0200

    Start setting up layout

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -4,8 +4,19 @@
 </script>
 
 <section class="home">
-Hello test!
+Hello world!
 </section>
+
+<style>
+  .home {
+    display: flex;
+    justify-content: center;
+    border-radius: 1rem;
+    align-items: center;
+    height: 90vh;
+    background-color: red;
+  }
+</style>
 
 <!-- <Canvas>
   <Scene />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,25 @@
 <script>
-    import '../app.css';
+	import '../app.css';
 </script>
 
-<nav>
-	<a class="text-3xl font-bold underline" href="/">Home</a>
-	<a href="/about">About</a>
-	<a href="/settings">Settings</a>
+<nav class="flex justify-between items-center bg-green-100 h-16 rounded-2xl mb-4 px-8">
+	<a href="https://github.com/ancientkingg" class="text-lg font-semibold">Samuel Bruin</a>
+	<div class="flex">
+		<a href="#" class="text-sm mr-6 bg-red-700">PROJECTS</a>
+		<a href="#" class="text-sm mr-6 bg-green-700">ABOUT</a>
+		<a href="#" class="text-sm bg-blue-700">CONTACT</a>
+	</div>
 </nav>
 
 <slot></slot>
 
-<style lang="css">
-    @import '/static/style/navbar.css';
+<style lang="postcss">
+	@import '/static/style/navbar.css';
+
+	:global(body) {
+		margin-top: 1.5rem;
+        margin-bottom: 1.5rem;
+        margin-left: 2rem;
+        margin-right: 2rem;
+	}
 </style>
