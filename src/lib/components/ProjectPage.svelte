0c3669be7600dff633b5d9cb4e59cb10336ad4d9
Samuel Bruin <S.bruin-1@student.tudelft.nl>
2025-01-28 21:26:52 +0100

    Add skeleton for project page

---
+++ src/lib/components/ProjectPage.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import Card from "./Card.svelte";
+
+</script>
+
+<section class="project-page">
+    <Card class="col-span-12 row-span-16"></Card>
+	<Card class="col-span-12 row-span-16"></Card>
+    <Card class="col-span-12 row-span-16"></Card>
+    <Card class="col-span-12 row-span-16"></Card>
+    <Card class="col-span-12 row-span-16"></Card>
+    <Card class="col-span-12 row-span-16"></Card>
+</section>
+
+<style lang="postcss">
+	:global(body) {
+		border-radius: 1.5rem;
+	}
+
+	.project-page {
+		display: grid;
+		grid-template-columns: repeat(36, minmax(0, 1fr));
+		grid-auto-rows: minmax(0, 1fr);
+
+		column-gap: 1.25rem;
+		row-gap: 1.25rem;
+
+		border-radius: 1.5rem;
+	}
+
+	@media only screen and (max-width: 1280px) {
+		.project-page {
+			grid-template-columns: repeat(24, minmax(0, 1fr));
+		}
+	}
+	@media only screen and (max-width: 768px) {
+		.project-page {
+			grid-template-columns: repeat(12, minmax(0, 1fr));
+		}
+	}
+</style>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import ProjectPage from '$lib/components/ProjectPage.svelte';
+</script>
+
+<ProjectPage />
