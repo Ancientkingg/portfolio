b22d419f73113845245e2e696a17a0f2956cbfcf
Samuel Bruin <S.bruin-1@student.tudelft.nl>
2025-12-14 01:00:30 +0100

    Remove log from measure function and stop resizing after first time

--- src/lib/actions/measure.ts
+++ src/lib/actions/measure.ts
@@ -1,7 +1,7 @@
 export function measure(node: HTMLElement, onResize: (rect: DOMRectReadOnly) => void) {
   const observer = new ResizeObserver(([entry]) => {
-    console.log('Resize observed:', entry.contentRect);
     onResize(entry.contentRect);
+    observer.disconnect();
   });
 
   observer.observe(node);
